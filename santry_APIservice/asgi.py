0a5fcf3ca4ec44eeccd132f37f25079876aa3a32
Pratik Bhattacharjee <pratikbhattacharjee09@gmail.com>
2025-02-01 20:11:20 +0800

    Initial Commit

---
+++ santry_APIservice/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for santry_APIservice project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'santry_APIservice.settings')
+
+application = get_asgi_application()
